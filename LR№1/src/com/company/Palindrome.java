d5f18f769095f7388bf03f9c7eca5659acf042ac
Vadim I <ignatov0131@gmail.com>
2021-03-01 15:24:03 +0300

    Добавление первой ЛР

---
+++ LR№1/src/com/company/Palindrome.java
@@ -0,0 +1,34 @@
+package com.company;
+
+// Нахождение палиндромов
+public class Palindrome {
+
+    public static void main(String[] args) {
+        String[] text = {"java", "Palindrome", "madam", "racecar", "apple", "kayak", "song", "noon"};
+
+        for(int i = 0; i < text.length; i++) {
+            String s = text[i];
+            if (isPalindrome(s)) {
+                System.out.println("Слово " + s + " палиндром.");
+            } else {
+                System.out.println("Слово " + s + " не палиндром.");
+            }
+        }
+    }
+
+    // Проверка на палиндром
+    public static boolean isPalindrome(String s) {
+        return s.equals(reverseString(s));
+    }
+
+    // Создание перевернутой строки
+    public static String reverseString(String inputString) {
+        String outputString = "";
+
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            outputString += inputString.charAt(i);
+        }
+
+        return outputString;
+    }
+}
