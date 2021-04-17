8c3ec2bfcbd355da51475428ef5cbf4b7a1157a9
Vadim-Matvei-Notebook <ignatov0131@gmail.com>
2021-04-17 15:21:03 +0300

    Добавление 5 тасков

--- Tasks/src/tasks/Task5.java
+++ Tasks/src/tasks/Task5.java
@@ -1,2 +1,206 @@
-package tasks;public class Task5 {
+package tasks;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Task5 {
+    /* Задача №1 */
+    public static boolean sameLetterPattern(String a, String b) {
+        if (a.length() != b.length()) return false;
+        HashMap<Character, Character> match = new HashMap<>();
+
+        for (int i = 0; i < a.length(); i++) {
+            if (!match.containsKey(a.charAt(i)))
+                match.put(a.charAt(i), b.charAt(i));
+            if (b.charAt(i) != match.get(a.charAt(i)))
+                return false;
+        }
+
+        return true;
+    }
+
+    /* Задача №2 */
+    public static String spiderVsFly(String spider, String fly) {
+        int sx = spider.charAt(0) - 65;
+        int sy = spider.charAt(1) - 48;
+        int fx = fly.charAt(0) - 65;
+        int fy = fly.charAt(1) - 48;
+
+        double strategyDist1 = sy + fy;
+        double strategyDist2 = Math.abs(sy - fy) + ((sx + fx) % 8) * fy * 0.76536686473;
+
+        String path = "";
+
+        if (strategyDist1 <= strategyDist2) {
+            for (int i = 0; i < sy; i++) {
+                path += spider.charAt(0);
+                path += sy - i;
+                path += '-';
+            }
+            path += "A0-";
+            for (int i = 0; i < fy; i++) {
+                path += fly.charAt(0);
+                path += i + 1;
+                path += '-';
+            }
+        } else {
+            for (int i = 0; i < Math.abs(sy - fy); i++) {
+                path += spider.charAt(0);
+                if (sy > fy) path += sy - i;
+                else path += sy + i;
+                path += '-';
+            }
+            for (int i = 0; i <= (sx + fx) % 8; i++) {
+                path += (char)(65 + (sx + i) % 8);
+                path += fly.charAt(1);
+                path += '-';
+            }
+        }
+
+        return path.substring(0, path.length() - 1);
+    }
+
+    /* Задача №3 */
+    public static int _digitsCount(long number) {
+        if (number == 0) return 0;
+        return 1 + _digitsCount(number / 10);
+    }
+
+    public static int digitsCount(long number) {
+        return 1 + _digitsCount(number / 10);
+    }
+
+
+    /* Задача №4 */
+    public static int[] getCharset(String word) {
+        int[] charset = new int[127];
+        for (char c : word.toCharArray()) charset[c]++;
+        return charset;
+    }
+
+    public static int totalPoints(String[] words, String scramble) {
+        int points = 0;
+        int[] scrambleCharset = getCharset(scramble);
+        for (int i = 0; i < words.length; i++) {
+            int[] wordCharset = getCharset(words[i]);
+            boolean good = true;
+            for (int j = 0; j < 127; j++)
+                if (wordCharset[j] > scrambleCharset[j]) {
+                    good = false;
+                    break;
+                }
+            if (good) {
+                points += words[i].length() - 2;
+                if (words[i].length() == 6) points += 50;
+            }
+        }
+        return points;
+    }
+
+    /* Задача №5 */
+    public static int longestRun(int[] arr) {
+        int max = 1;
+        int cur = 1;
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i+1] - arr[i] == 1 || arr[i+1] - arr[i] == -1) {
+                cur++;
+                if (max < cur) max = cur;
+            } else cur = 1;
+        return max;
+    }
+
+    /* Задача №6 */
+    public static String takeDownAverage(String[] percents) {
+        int avg = 0;
+        for (String s : percents)
+            avg += Integer.parseInt(s.substring(0, s.length() - 1));
+        return (avg / percents.length - percents.length * 5 - 5) + "%";
+    }
+
+    /* Задача №7 */
+    public static String rearrange(String str) {
+        String[] words = str.split(" ");
+        String[] res = new String[words.length];
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                if (Character.isDigit(word.charAt(i))) {
+                    res[word.charAt(i) - 48 - 1] = word.substring(0, i) + word.substring(i+1);
+                    break;
+                }
+            }
+        }
+        return String.join(" ", res);
+    }
+
+    /* Задача №8 */
+    public static Integer[] splitNumber(int n) {
+        ArrayList<Integer> res = new ArrayList<>();
+        while (n > 0) {
+            res.add(n % 10);
+            n /= 10;
+        }
+        return res.toArray(new Integer[res.size()]);
+    }
+
+    public static int maxPossible(int n, int r) {
+        Integer[] num = splitNumber(n);
+        Integer[] rnum = splitNumber(r);
+        Arrays.sort(rnum, Collections.reverseOrder());
+        int rnumindex = 0;
+        for (int i = num.length - 1; i >= 0; i--) {
+            if (num[i] < rnum[rnumindex]) {
+                num[i] = rnum[rnumindex];
+                rnumindex++;
+            }
+            if (rnumindex == rnum.length)
+                break;
+        }
+        n = 0;
+        int dec = 1;
+        for (int i : num) {
+            n += i * dec;
+            dec *= 10;
+        }
+        return n;
+    }
+
+    /* Задача №9 */
+    public static SimpleDateFormat parseDate = new SimpleDateFormat("MMMM d, yyyy HH:mm");
+    public static SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-M-d HH:mm");
+
+    public static String getGMT(String city) {
+        if (city == "Los Angeles") return "GMT-08:00";
+        if (city == "New York") return "GMT-05:00";
+        if (city == "Caracas") return "GMT- 04:30";
+        if (city == "Buenos Aires") return "GMT-03:00";
+        if (city == "London") return "GMT00:00";
+        if (city == "Rome") return "GMT+01:00";
+        if (city == "Moscow") return "GMT+03:00";
+        if (city == "Tehran") return "GMT+03:30";
+        if (city == "New Delhi") return "GMT+05:30";
+        if (city == "Beijing") return "GMT+08:00";
+        if (city == "Canberra") return "GMT+10:00";
+        return "GMT";
+    }
+
+    public static String timeDifference(
+            String cityA, String timestamp, String cityB
+    ) {
+        try {
+            parseDate.setTimeZone(TimeZone.getTimeZone(getGMT(cityA)));
+            formatDate.setTimeZone(TimeZone.getTimeZone(getGMT(cityB)));
+            Date date = parseDate.parse(timestamp);
+            return formatDate.format(date);
+        } catch(Exception e) {}
+        return null;
+    }
+
+    /* Задача №10 */
+    public static boolean isNew(int n) {
+        Integer[] num = splitNumber(n);
+        for (int i = 0; i < num.length - 1; i++)
+            if (num[i] > 0 && num[i] < num[num.length - 1])
+                return false;
+        return true;
+    }
 }
